43336e7b1b6b39b5625c642fe7e5df90b3af8c23
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 21:01:36 +0530

    Update ElectronicColorCode.h

--- ElectronicColorCode.h
+++ ElectronicColorCode.h
@@ -1,3 +1,6 @@
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
