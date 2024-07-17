74d5095fe30d8059c9413e459ab3cf854567137c
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 22:03:14 +0530

    Update ColorCodeReferenceManual.h

--- ColorCodeReferenceManual.h
+++ ColorCodeReferenceManual.h
@@ -1,5 +1,2 @@
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-
 void PrintColorCodingReference();
