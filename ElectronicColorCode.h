5208c8215de5a44608b126722799ac6258fc200f
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 20:31:46 +0530

    Create ElectronicColorCode.h

---
+++ ElectronicColorCode.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
