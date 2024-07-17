f7dc34e18849dbe891af4653243cd0b0652edbc6
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 20:50:08 +0530

    Update ElectronicColorCode.c

--- ElectronicColorCode.c
+++ ElectronicColorCode.c
@@ -2,6 +2,8 @@
 #include <assert.h>
 #include "ElectronicColorCode.h"
 #include "DefineColorNames.h"
+#include "NumberToPair_Test.h"
+#include "PairToNumber_Test.h"
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
@@ -24,31 +26,6 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
-}
-
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
-{
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-}
-
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
 }
 
 int main() {
