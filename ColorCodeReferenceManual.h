86f45f2c45500b2582cec1b870cbaaf33ee7cf9f
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 21:54:36 +0530

    Update ColorCodeReferenceManual.h

--- ColorCodeReferenceManual.h
+++ ColorCodeReferenceManual.h
@@ -2,4 +2,7 @@
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 void PrintColorCodingReference();
