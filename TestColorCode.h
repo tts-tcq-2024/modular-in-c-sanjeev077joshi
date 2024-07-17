c46df8ade9533719cf548799aefbb42f5f67b90a
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 20:53:07 +0530

    Create TestColorCode.h

---
+++ TestColorCode.h
@@ -0,0 +1,11 @@
+#include"ElectronicColorCode.h"
+
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
+
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
