072c28f7d05a9d9a5313646b20413ea20bc7bcaf
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 20:59:42 +0530

    Update ElectronicColorCode.h

--- ElectronicColorCode.h
+++ ElectronicColorCode.h
@@ -1,9 +1,8 @@
-#include <stdlib.h>
-
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
