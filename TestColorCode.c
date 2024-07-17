8219587a9101c53bfc563d40ee0109f37b0e9cec
sanjeev077joshi <52950883+sanjeev077joshi@users.noreply.github.com>
2024-07-17 20:54:49 +0530

    Update TestColorCode.c

--- TestColorCode.c
+++ TestColorCode.c
@@ -1,5 +1,8 @@
 #include"TestColorCode.h"
 
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor)
